c9f661cf921ba42c48cf947116510dcdb1a9da3e
DLMousey <tom@deadlyllama.com>
2019-09-22 05:38:35 +0100

    re-style registration component

--- src/modules/auth/components/register/register.component.ts
+++ src/modules/auth/components/register/register.component.ts
@@ -4,7 +4,7 @@ import { AuthService } from '../../../shared/services/auth.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
 
