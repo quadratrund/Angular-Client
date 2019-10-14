7f80f3500b44f162335d466a42ec4dd86f21573e
quadratrund <56112624+quadratrund@users.noreply.github.com>
2019-10-14 17:22:09 +0200

    fix NotebookDetailComponent test

--- src/modules/notebooks/components/notebook-detail/notebook-detail.component.spec.ts
+++ src/modules/notebooks/components/notebook-detail/notebook-detail.component.spec.ts
@@ -1,5 +1,9 @@
+import { HttpClientModule } from '@angular/common/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { AppRoutingModule } from '../../../../app/app-routing.module';
+import { LandingComponent } from '../../../../app/components/landing/landing.component';
+import { NotebookService } from '../../services/notebook.service';
 import { NotebookDetailComponent } from './notebook-detail.component';
 
 describe('NotebookDetailComponent', () => {
@@ -8,12 +12,25 @@ describe('NotebookDetailComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NotebookDetailComponent ]
+      declarations: [ NotebookDetailComponent, LandingComponent ],
+      imports: [ AppRoutingModule, HttpClientModule ],
+      providers: [ NotebookService ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    const notebookService: NotebookService = TestBed.get(NotebookService);
+    notebookService.setRouteStorage({
+      id: 42,
+      name: 'Some Name',
+      notes: [
+        {
+          title: 'Some Title',
+          content: 'Some Content'
+        }
+      ]
+    });
     fixture = TestBed.createComponent(NotebookDetailComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
